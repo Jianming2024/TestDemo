ddbfb5be8a77a102c8cb879da19e33417df31056
katja981 <katstr01@easv365.dk>
2024-11-13 10:05:00 +0100

    Change in HelloController

--- src/main/java/easv/dk/testdemo/HelloController.java
+++ src/main/java/easv/dk/testdemo/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Hello Everybody!");
+        welcomeText.setText("Hello Everybody :)!");
     }
 }
